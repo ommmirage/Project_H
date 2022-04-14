b337fe70d8f792f589d026d57d2e354f7fae77f4
Sergey Miroshnichenko <ommmirage@gmail.com>
2022-04-14 12:24:54 +0400

    Zoom fix

--- Assets/Scripts/CameraMouseController.cs
+++ Assets/Scripts/CameraMouseController.cs
@@ -13,19 +13,16 @@ public class CameraMouseController : MonoBehaviour
         Ray mouseRay = Camera.main.ScreenPointToRay (Input.mousePosition);
 		// What is the point at which the mouse ray intersects Y=0
 		if (mouseRay.direction.y >= 0) {
-			//Debug.LogError("Why is mouse pointing up?");
 			return;
 		}
 		float rayLength = (mouseRay.origin.y / mouseRay.direction.y);
 		Vector3 hitPos = mouseRay.origin - (mouseRay.direction * rayLength);
 
 		if (Input.GetMouseButtonDown (0)) {
-			// Mouse button just went down -- start a drag.
 			isDraggingCamera = true;
 
 			lastMousePosition = hitPos;
 		} else if (Input.GetMouseButtonUp (0)) {
-			// Mouse button went up, stop drag
 			isDraggingCamera = false;
 		}
 
@@ -51,24 +48,24 @@ public class CameraMouseController : MonoBehaviour
 
 		Vector3 p = Camera.main.transform.position;
 
-		// Stop zooming out at a certain distance.
-		// TODO: Maybe you should still slide around at 20 zoom?
-		if (scrollAmount > 0 || p.y < (maxHeight - 0.1f)) {
-			cameraTargetOffset += dir * scrollAmount;
-		}
-		Vector3 lastCameraPosition = Camera.main.transform.position;
-		Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, Camera.main.transform.position + cameraTargetOffset, Time.deltaTime * 5f);
-		cameraTargetOffset -= Camera.main.transform.position - lastCameraPosition;
-
+			// Stop zooming out at a certain distance.
+			// TODO: Maybe you should still slide around at 20 zoom?
+			if (scrollAmount > 0 || p.y < (maxHeight - 0.1f)) {
+				cameraTargetOffset += dir * scrollAmount;
+			}
+			Vector3 lastCameraPosition = Camera.main.transform.position;
+			Camera.main.transform.position = Vector3.Lerp(
+											Camera.main.transform.position, 
+											Camera.main.transform.position + cameraTargetOffset, 
+											Time.deltaTime * 5f
+											);
+			cameraTargetOffset -= Camera.main.transform.position - lastCameraPosition;
 
 		p = Camera.main.transform.position;
-		if (p.y < minHeight) {
-			p.y = minHeight;
+		if ((p.y < minHeight) || (p.y > maxHeight)) 
+		{
+			Camera.main.transform.position = lastCameraPosition;
 		}
-		if (p.y > maxHeight) {
-			p.y = maxHeight;
-		}
-		Camera.main.transform.position = p;
 
 		// Change camera angle
 		float lowZoom = minHeight + 3;
